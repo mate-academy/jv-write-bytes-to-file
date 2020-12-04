b927a7d77c5efb660892712b124c02272ff10fb2
Serhii Semenykhin <semyonich@users.noreply.github.com>
2020-12-04 17:28:18 +0200

    work is done

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-
+        File file = new File(fileName);
+        try {
+            Files.write(file.toPath(), data);
+        } catch (IOException e) {
+            throw new RuntimeException("Unable write to file:" + fileName, e);
+        }
     }
 }
