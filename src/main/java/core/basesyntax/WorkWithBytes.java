96fe84972c24964c0f461e686b1b7ef61ffcdc9a
Okulov Vadim <Vedroidua@gmail.com>
2020-12-04 21:29:30 +0200

    Fix

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -7,8 +7,8 @@ import java.io.IOException;
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
-            for (byte b : data) {
-                bufferedWriter.write((char) b);
+            for (byte byteFromData : data) {
+                bufferedWriter.write(byteFromData);
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file " + fileName, e);
