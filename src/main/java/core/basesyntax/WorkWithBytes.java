731f4c2c490629a781e30eecc1bab9ec3321bb29
Volodymyr Lykhvar <vlykhvar@gmail.com>
2020-12-04 19:54:33 +0200

    hw3 v1.0.0 has written writeBytesToFile

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,7 +1,21 @@
 package core.basesyntax;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-
+        File file = new File(fileName);
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+            for(byte info: data) {
+                bufferedWriter.write(info);
+            }
+            bufferedWriter.close();
+        } catch (IOException e) {
+            System.out.println("Can't write to this file");
+        }
     }
 }
