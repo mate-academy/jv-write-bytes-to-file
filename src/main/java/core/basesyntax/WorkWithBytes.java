0b77fe1fd498eaf0d78d383890dbb6b8551b34fe
Yura <yuramitryahin@gmail.com>
2020-12-04 21:38:22 +0200

    Create FileOutputStream and write byte array in file

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,7 +1,18 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-
+        try (FileOutputStream fileOutputStream = new FileOutputStream(new File(fileName))) {
+            fileOutputStream.write(data);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't find file with this name!", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to this file!", e);
+        }
     }
 }
