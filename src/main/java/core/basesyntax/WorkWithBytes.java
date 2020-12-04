5b7f6b8e1af4002db0e2c2d74355de43ce449a2f
Fedenko Maksym <fedenko.maxim.a@gmail.com>
2020-12-04 19:15:34 +0200

    added solution

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,7 +1,20 @@
 package core.basesyntax;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-
+        File file = new File(fileName);
+        try {
+            file.createNewFile();
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+            Files.write(file.toPath(), data);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't create a file.", e);
+        }
     }
 }
