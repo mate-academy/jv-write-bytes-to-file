6b6562dedce51aab31ac9a62155a26e1e6018d8f
nikelok01@gmail.com <163amefeZ>
2020-12-04 13:33:13 +0200

    created functional of method writeBytesToFile

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-
+        try (BufferedWriter writeFile = new BufferedWriter(new FileWriter(fileName + ""))) {
+            for (byte bytes : data) {
+                writeFile.write(bytes);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't create a file!" + fileName, e);
+        }
     }
 }
