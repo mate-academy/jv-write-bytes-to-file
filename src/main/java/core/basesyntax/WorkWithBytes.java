dc564b60d6029d1473906b16d27f382d023fe3c0
Alex <65593125+Artificial-Friend@users.noreply.github.com>
2020-12-04 12:23:26 +0200

    solved task

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,7 +1,21 @@
 package core.basesyntax;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-
+        File file = new File(fileName);
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+            for (byte byteFromData : data) {
+                bufferedWriter.write((char) byteFromData);
+                bufferedWriter.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
