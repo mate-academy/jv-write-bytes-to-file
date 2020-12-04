6ff0920e24c1bb223f40e3fcb9bcc3001b7f58f7
Roman Saviak <saviak.roman@gmail.com>
2020-12-04 18:20:45 +0200

    Byte code to file

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,7 +1,17 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-
+        File file = new File(fileName);
+        try {
+            FileOutputStream byteInfo = new FileOutputStream(file);
+            byteInfo.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException("This file can't exist", e);
+        }
     }
 }
