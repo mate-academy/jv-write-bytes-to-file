4ee6c6ce3b0a125fae1544bf72f5818b0c98da52
Likh-Alex <likhachov991@protonmail.com>
2020-12-05 13:44:19 +0200

    Solution attempt

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,7 +1,16 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-
+        File fileTo = new File(fileName);
+        try {
+            Files.write(fileTo.toPath(), data);
+        } catch (IOException e) {
+            throw new RuntimeException("Can not write to file");
+        }
     }
 }
