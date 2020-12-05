ea420545d2a59162756ca3cb3df781ae69625270
YuraBondarenko <yuriybondarenko2001@gmail.com>
2020-12-05 14:14:54 +0200

    write file with byte array

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,7 +1,20 @@
 package core.basesyntax;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-
+        File file = new File(fileName);
+        for (byte datas : data) {
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName,
+                    true))) {
+                bufferedWriter.write(datas);
+            } catch (IOException e) {
+                throw new RuntimeException("Can't write file" + fileName, e);
+            }
+        }
     }
 }
