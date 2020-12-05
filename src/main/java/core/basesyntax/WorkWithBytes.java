5cbc1800f571db603eadfe10796441d35845ae9b
ilya-brinchuk <ilya.brinchuk@gmail.com>
2020-12-05 18:11:47 +0200

    added logic in method writeBytesToFile

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,7 +1,22 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
+        try {
+            File file = new File(fileName);
+            file.createNewFile();
+            Path path = Paths.get(fileName);
+            Files.write(path, data);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file " + fileName, e);
+        }
 
     }
 }
