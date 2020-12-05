3eff2ea7b4e7cc509f11ed0d66177d91a252d2d9
antonpanchishko <PanchishkoMayor@gmail.com>
2020-12-05 14:01:35 +0200

    my solution

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,7 +1,23 @@
 package core.basesyntax;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
+        File file = new File(fileName);
+        BufferedWriter bufferedWriter = null;
 
+        for (byte bytes: data) {
+            try {
+                bufferedWriter = new BufferedWriter(new FileWriter(file, true));
+                bufferedWriter.write(bytes);
+                bufferedWriter.close();
+            } catch (IOException e) {
+                throw new RuntimeException("Can't write ti file");
+            }
+        }
     }
 }
