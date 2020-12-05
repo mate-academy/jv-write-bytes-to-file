73e70261e2849217d349377859972039b4dd476d
Baldinini <baldinat063@gmail.com>
2020-12-05 15:17:26 +0200

    task solved

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-
+        try (FileOutputStream writeByte = new FileOutputStream(fileName)) {
+            writeByte.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't find file" + fileName, e);
+        }
     }
 }
