183a5a8a5a0dcdcc8092f4e001eeae1192a8805c
Tonitrus <grom4er@gmail.com>
2020-12-06 20:45:50 +0200

    Complete task and test pass

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-
+        try (FileOutputStream out = new FileOutputStream(fileName)) {
+            out.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException("Problem to write file", e);
+        }
     }
 }
