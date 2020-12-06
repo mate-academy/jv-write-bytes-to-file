985eee4d819269f0ee8564e49bb32c59ce791624
egorka <entertainment.veks@gmail.com>
2020-12-06 17:59:32 +0200

    yegor sereda 1st commit

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,7 +1,22 @@
 package core.basesyntax;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
+        File file = new File(fileName);
+        try {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+            for (byte bt : data) {
+                bufferedWriter.write(bt);
+            }
+            bufferedWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file " + fileName, e);
+        }
 
     }
 }
