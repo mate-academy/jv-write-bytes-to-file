71ddb4ea8e9d3947bb358f28f848b3f788f4c151
kerrender <agreed_note@hotmail.com>
2020-12-06 22:35:23 +0200

    Initial

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,7 +1,18 @@
 package core.basesyntax;
 
-public class WorkWithBytes {
-    public void writeBytesToFile(String fileName, byte[] data) {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithBytes {
+
+    public void writeBytesToFile(String fileName, byte[] data) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));) {
+            for (byte piece: data) {
+                writer.write(piece);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to file", e);
+        }
     }
 }
