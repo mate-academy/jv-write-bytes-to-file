14fae0ad800ec6a9e7ecc43d412d4c7818e9e490
Igor Bochkor <igorbochkor@gmail.com>
2020-12-06 11:31:09 +0200

    added method createEmptyFile

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -7,13 +7,26 @@ import java.io.IOException;
 
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-        File file = new File(fileName);
+        File file = createEmptyFile(fileName);
         for (byte iterator : data) {
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file,true))) {
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
                 bufferedWriter.write(iterator);
             } catch (IOException e) {
                 throw new RuntimeException("Can't write data to file " + fileName, e);
             }
         }
+    }
+
+    public File createEmptyFile(String fileName) {
+        File file = new File(fileName);
+        if (file.exists()) {
+            file.delete();
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException("Can't create file ", e);
+            }
+        }
+        return file;
     }
 }
