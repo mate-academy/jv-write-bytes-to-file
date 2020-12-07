31cb53e4e00eab0fbdec863840b70c48e5b85a28
ShemsheyMaxim <shanhaymax@gmail.com>
2020-12-07 21:59:06 +0200

    deleted method create file

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,6 +1,5 @@
 package core.basesyntax;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,15 +11,5 @@ public class WorkWithBytes {
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file " + Path.of(fileName), e);
         }
-    }
-
-    private File createFile(String fileName) {
-        File file = new File(fileName);
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException("Can't create file" + file, e);
-        }
-        return file;
     }
 }
