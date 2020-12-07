6f1eec782c974a9419efdc1d37537d079fdd5c0f
Yaroslav <yaroslav8oroboro@gmail.com>
2020-12-07 11:30:13 +0200

    added new solution using FileOutputStream

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,26 +1,17 @@
 package core.basesyntax;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class WorkWithBytes {
 
     public void writeBytesToFile(String fileName, byte[] data) {
-        File newFile = new File(fileName);
         try {
-            boolean success = newFile.createNewFile();
+            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+            fileOutputStream.write(data);
         } catch (IOException e) {
-            throw new RuntimeException("Can't create file", e);
-        }
-        for (Byte newByte : data) {
-            try (BufferedWriter bufferedWriter
-                         = new BufferedWriter(new FileWriter(newFile, true))) {
-                bufferedWriter.write(newByte);
-            } catch (IOException e) {
-                throw new RuntimeException("Can't write data to file", e);
-            }
+            throw new RuntimeException("Can't create file");
         }
     }
 }
+
