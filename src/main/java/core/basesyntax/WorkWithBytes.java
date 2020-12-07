23e15439c8da3128b93083cb8b9caf994bed9d63
nikelok01@gmail.com <163amefeZ>
2020-12-07 20:04:42 +0200

    - changed name of variable "bytes" to "singleByte"

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -7,8 +7,8 @@ import java.io.IOException;
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
         try (BufferedWriter writeFile = new BufferedWriter(new FileWriter(fileName))) {
-            for (byte bytes : data) {
-                writeFile.write(bytes);
+            for (byte singleByte : data) {
+                writeFile.write(singleByte);
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't create a file!" + fileName, e);
