11aa1803b26745afd6a7bd4f90298b315325d2d4
Yaroslav <yaroslav8oroboro@gmail.com>
2020-12-07 14:21:01 +0200

    added closing of the stream

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -6,8 +6,7 @@ import java.io.IOException;
 public class WorkWithBytes {
 
     public void writeBytesToFile(String fileName, byte[] data) {
-        try {
-            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+        try (FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
             fileOutputStream.write(data);
         } catch (IOException e) {
             throw new RuntimeException("Can't create file", e);
