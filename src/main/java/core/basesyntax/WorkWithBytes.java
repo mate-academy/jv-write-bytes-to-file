a82b0f165978bd966492c88f240ce53109421798
Alekearl <alexandrhurko@gmail.com>
2020-12-07 11:50:54 +0200

    implement method writeBytesToFile using FileOutPutStream

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,23 +1,16 @@
 package core.basesyntax;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
         File file;
-        try {
-            file = new File(fileName);
-            file.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException("Can't create file", e);
-        }
-
+        file = new File(fileName);
         for (Byte oneByte : data) {
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
-                bufferedWriter.write(oneByte);
+            try (FileOutputStream fileOutputStream = new FileOutputStream(file, true)) {
+                fileOutputStream.write(oneByte);
             } catch (IOException e) {
                 throw new RuntimeException("Can't write data to file!", e);
             }
