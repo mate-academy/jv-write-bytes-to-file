63749ac2a5f4eb960593d6e9f54d516519d08df1
RuslanIbIsh <ruslan8ishankulov@gmail.com>
2020-12-07 11:58:15 +0200

    impl suggestions: move file.createNF, deleted Append

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -3,18 +3,13 @@ package core.basesyntax;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.StandardOpenOption;
 
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
         File file = new File(fileName);
         try {
             file.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException("Can't create file", e);
-        }
-        try {
-            Files.write(file.toPath(), data, StandardOpenOption.APPEND);
+            Files.write(file.toPath(), data);
         } catch (IOException e) {
             throw new RuntimeException("Can't write data to file", e);
         }
