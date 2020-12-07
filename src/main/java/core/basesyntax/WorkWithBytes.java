d71c096868b1301eebbb20fea8e9e04a95c4138e
Alekearl <alexandrhurko@gmail.com>
2020-12-07 15:03:54 +0200

    refactored method (without forEach loop) writeBytesToFile using FileOutPutStream

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -6,13 +6,11 @@ import java.io.IOException;
 
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-        for (Byte oneByte : data) {
-            try (FileOutputStream fileOutputStream =
-                         new FileOutputStream(new File(fileName), true)) {
-                fileOutputStream.write(oneByte);
-            } catch (IOException e) {
-                throw new RuntimeException("Can't write data to file!", e);
-            }
+        try (FileOutputStream fileOutputStream =
+                     new FileOutputStream(new File(fileName), true)) {
+            fileOutputStream.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write data to file!", e);
         }
     }
 }
