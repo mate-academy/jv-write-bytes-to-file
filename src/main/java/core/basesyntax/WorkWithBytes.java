56d09b8a687edf2bad627d416f88cf38dfff02c6
Fedenko Maksym <fedenko.maxim.a@gmail.com>
2020-12-07 20:53:09 +0200

    deleted redundant actions

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,18 +1,13 @@
 package core.basesyntax;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-        File file = new File(fileName);
         try {
-            file.createNewFile();
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
-            Files.write(file.toPath(), data);
+            Files.write(Path.of(fileName), data);
         } catch (IOException e) {
             throw new RuntimeException("Can't create a file!", e);
         }
