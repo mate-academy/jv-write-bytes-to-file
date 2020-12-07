1ae6aa285d437ffa40562934cba019b4d5d956dc
Yaroslav <yaroslav8oroboro@gmail.com>
2020-12-07 02:21:46 +0200

    added file creation & bytes to file implementation

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,7 +1,26 @@
 package core.basesyntax;
 
-public class WorkWithBytes {
-    public void writeBytesToFile(String fileName, byte[] data) {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class WorkWithBytes {
+
+    public void writeBytesToFile(String fileName, byte[] data) {
+        File newFile = new File(fileName);
+        try {
+            boolean success = newFile.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException("Can't create file", e);
+        }
+        for (Byte newByte : data) {
+            try (BufferedWriter bufferedWriter
+                         = new BufferedWriter(new FileWriter(newFile, true))) {
+                bufferedWriter.write(newByte);
+            } catch (IOException e) {
+                throw new RuntimeException("Can't write data to file", e);
+            }
+        }
     }
 }
