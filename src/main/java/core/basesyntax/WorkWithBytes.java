db9fbb4dc241b0f7266a7985ef89f9a42443d634
YuraBondarenko <yuriybondarenko2001@gmail.com>
2020-12-07 11:36:01 +0200

    used "FileOutputStream" instead of "BufferedWriter" write file with byte array

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,20 +1,14 @@
 package core.basesyntax;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-        File file = new File(fileName);
-        for (byte datas : data) {
-            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName,
-                    true))) {
-                bufferedWriter.write(datas);
-            } catch (IOException e) {
-                throw new RuntimeException("Can't write file" + fileName, e);
-            }
+        try (FileOutputStream bufferedWriter = new FileOutputStream(fileName)) {
+            bufferedWriter.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write file" + fileName, e);
         }
     }
 }
