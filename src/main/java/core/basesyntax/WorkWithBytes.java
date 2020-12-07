2636b4f029fb5ae6465470087edb9479cfc74ff6
Sashk52 <a.tsven@gmail.com>
2020-12-07 20:00:09 +0200

    added whitespaces twice

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -7,9 +7,9 @@ import java.nio.file.Path;
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
         try {
-            Files.write(Path.of(fileName),data);
+            Files.write(Path.of(fileName), data);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data to file",e);
+            throw new RuntimeException("Can't write data to file", e);
         }
     }
 }
