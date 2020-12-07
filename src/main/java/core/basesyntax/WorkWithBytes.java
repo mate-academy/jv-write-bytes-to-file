735e6b421a67c37609ff7183b055b360fa3ba42d
antoniukivan <ivan.antonuk1992@gmail.com>
2020-12-07 12:07:13 +0200

    changed BufferedWriter to FileOutputStream

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,17 +1,14 @@
 package core.basesyntax;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
-            for (byte symbol : data) {
-                writer.write(symbol);
-            }
+        try (FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
+            fileOutputStream.write(data);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data to file");
+            throw new RuntimeException("Can't write data to file", e);
         }
     }
 }
