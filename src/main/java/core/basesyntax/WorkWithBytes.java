72d48725c963095bc3dfe73382437251b501e18d
Volodymyr Lykhvar <vlykhvar@gmail.com>
2020-12-07 14:42:18 +0200

    added FileOutputStream deleted bufferReader

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,17 +1,14 @@
 package core.basesyntax;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
         File file = new File(fileName);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
-            for (byte info: data) {
-                bufferedWriter.write(info);
-            }
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+            fileOutputStream.write(data);
         } catch (IOException e) {
             System.out.println("Can't write to the file");
         }
