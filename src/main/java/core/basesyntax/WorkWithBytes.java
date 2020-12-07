5dd12d73afae5109b43d5d36009b5e0ef59fd467
antonpanchishko <PanchishkoMayor@gmail.com>
2020-12-07 16:02:07 +0200

    2) solve FileOutputStream

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,23 +1,14 @@
 package core.basesyntax;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-        File file = new File(fileName);
-        BufferedWriter bufferedWriter = null;
-
-        for (byte bytes: data) {
-            try {
-                bufferedWriter = new BufferedWriter(new FileWriter(file, true));
-                bufferedWriter.write(bytes);
-                bufferedWriter.close();
-            } catch (IOException e) {
-                throw new RuntimeException("Can't write ti file");
-            }
+        try (FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
+            fileOutputStream.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException("Cant write to file " + fileName, e);
         }
     }
 }
