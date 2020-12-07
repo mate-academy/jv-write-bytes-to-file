f179c6ae148554eb41730cd243580dd93d03f1b1
m.kislukhina <marharita.kislukhina@gmail.com>
2020-12-07 14:11:05 +0100

    FileOutputStream used to write byte array.

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,7 +1,14 @@
 package core.basesyntax;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-
+        try (FileOutputStream myBytes = new FileOutputStream(fileName)) {
+            myBytes.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write bytes to file", e);
+        }
     }
 }
