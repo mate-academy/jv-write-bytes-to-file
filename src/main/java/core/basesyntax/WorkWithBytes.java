c640da316b6daddc0ff9ff8379002e5f37d429e9
Alekearl <alexandrhurko@gmail.com>
2020-12-07 14:23:58 +0200

    refactored method writeBytesToFile using FileOutPutStream

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -6,10 +6,9 @@ import java.io.IOException;
 
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-        File file;
-        file = new File(fileName);
         for (Byte oneByte : data) {
-            try (FileOutputStream fileOutputStream = new FileOutputStream(file, true)) {
+            try (FileOutputStream fileOutputStream =
+                         new FileOutputStream(new File(fileName), true)) {
                 fileOutputStream.write(oneByte);
             } catch (IOException e) {
                 throw new RuntimeException("Can't write data to file!", e);
