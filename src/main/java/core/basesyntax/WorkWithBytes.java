8a07dfbd63fe5d2826fdf5a2891f40532b06555e
KostyantynProskura <kostyantyn.proskura@gmail.com>
2020-12-07 14:59:49 +0200

    2) Commit - deleted 9-14 lines and changed 12 line from  file.toPath(), data to Path.of(fileName), data...

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,20 +1,15 @@
 package core.basesyntax;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-        File file = new File(fileName);
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException("Can't create file!", e);
-        }
+
         for (byte tempByte : data) {
             try {
-                Files.write(file.toPath(), data);
+                Files.write(Path.of(fileName), data);
             } catch (IOException e) {
                 throw new RuntimeException("Can't write data to file " + fileName, e);
             }
