319555f5b97becd4d7ef0b24f84a84cb00070a2e
AndreiYazadzhi <belator251000@gmail.com>
2020-12-07 19:56:56 +0200

    Sry.Changes didn't pushed

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -9,10 +9,9 @@ public class WorkWithBytes {
 
     public void writeBytesToFile(String fileName, byte[] data) {
         File file = new File(fileName);
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
             for (byte byteData : data) {
                 bufferedWriter.write(byteData);
-                bufferedWriter.flush();
             }
         } catch (IOException e) {
             throw new RuntimeException("Can't correctly read data from file " + fileName, e);
