00820e9185a83ae8be7187d15adea998ca5e8610
kerrender <agreed_note@hotmail.com>
2020-12-07 20:41:28 +0200

    fix: line 10 incantation issue

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -7,7 +7,7 @@ public class WorkWithBytes {
 
     public void writeBytesToFile(String fileName, byte[] data) {
         try (FileOutputStream writer = new FileOutputStream(fileName)) {
-                writer.write(data);
+            writer.write(data);
         } catch (IOException e) {
             throw new RuntimeException("Can't write to file" + fileName, e);
         }
