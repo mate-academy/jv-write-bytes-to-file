b3041619e3965ac58472a4bc4ad997e72521f75f
sanek98cv <73754789+sanek98cv@users.noreply.github.com>
2020-12-07 11:42:36 +0200

    Fixed: added the original exception

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,5 +1,6 @@
 package core.basesyntax;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -7,11 +8,10 @@ public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
         try (FileOutputStream writeToFile = new FileOutputStream(fileName)) {
             writeToFile.write(data);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Can't creates a file output stream", e);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write data to file");
+            throw new RuntimeException("Can't write data to file", e);
         }
     }
 }
-
-
-
