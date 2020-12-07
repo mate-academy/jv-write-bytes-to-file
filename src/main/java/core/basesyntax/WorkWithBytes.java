f1cf3b93c921c8309e91e85d130658f80a5adf07
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-07 11:54:05 +0200

    .createNewFile with try-catch surrounding was removed

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -8,12 +8,6 @@ import java.io.IOException;
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
         File fileToWrite = new File(fileName);
-        try {
-            fileToWrite.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException("File "
-                    + fileToWrite.getName() + " couldn't be created", e);
-        }
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileToWrite))) {
             for (byte dataLine : data) {
                 bw.write(dataLine);
