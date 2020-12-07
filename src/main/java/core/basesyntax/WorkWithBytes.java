838a4f54b56dcaa3a8c9c63d943e0c5d8e0ad53f
Ivan Marchenko <51205960+IwalkInSilence@users.noreply.github.com>
2020-12-07 16:09:02 +0200

    remove for loop and add FileOutputStream package

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,21 +1,14 @@
 package core.basesyntax;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-        File file = new File(fileName);
-        for (byte bytes: data) {
-            try (BufferedWriter bufferedWriter
-                         = new BufferedWriter(new FileWriter(fileName, true))) {
-                bufferedWriter.write(bytes);
-            } catch (IOException e) {
-                throw new RuntimeException("Can't write data to file - " + fileName, e);
-            }
+        try (FileOutputStream outData = new FileOutputStream(fileName)) {
+            outData.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException("Problem to write file - " + fileName, e);
         }
     }
 }
-
