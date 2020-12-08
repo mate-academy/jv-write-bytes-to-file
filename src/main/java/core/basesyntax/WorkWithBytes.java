e40b0d45ba3c459f1dd6e6038a01acde2a336ce0
ivlytvynets <ivlytvynets@users.noreply.github.com>
2020-12-08 02:07:46 +0200

    writeBytesToFile was implemented

--- src/main/java/core/basesyntax/WorkWithBytes.java
+++ src/main/java/core/basesyntax/WorkWithBytes.java
@@ -1,7 +1,15 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class WorkWithBytes {
     public void writeBytesToFile(String fileName, byte[] data) {
-
+        try {
+            Files.write(Paths.get(fileName), data);
+        } catch (IOException e) {
+            throw new RuntimeException("Can't write to the file " + fileName, e);
+        }
     }
 }
